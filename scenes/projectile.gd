4ce1e797767679a99a33ab6bc39be3a17eb5c774
tianlongc <tianlongchoo@gmail.com>
2026-04-16 20:46:24 +0800

    feat: create TurretNode and successfully spawn projectiles

--- scenes/projectile.gd
+++ scenes/projectile.gd
@@ -1,11 +1,23 @@
 extends Area2D
+class_name Projectile
 
+@export var speed: float = 200.0
+var direction: Vector2 = Vector2.LEFT # Default shooting left
+var source: Node # Tracks who fired it
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta):
+	position += direction * speed * delta
 
+# Connect the screen_exited signal from VisibleOnScreenNotifier2D
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# CTRL+C
+func get_clipboard_data() -> ClipboardData:
+	var data = ClipboardData.new()
+	data.type = "projectile"
+	data.data = {
+		"speed": speed,
+		"original_direction": direction
+	}
+	return data
